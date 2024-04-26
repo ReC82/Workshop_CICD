02046cd7181dff791e4612eb6026932217406372
ReC82 <lloyd.malfliet@gmail.com>
2024-04-26 11:48:24 +0200

    update playbook script

--- _FULL_DEPLOY/Azure/scripts/controller/control_ansible_play.sh
+++ _FULL_DEPLOY/Azure/scripts/controller/control_ansible_play.sh
@@ -1,3 +1,3 @@
 hosts=$1
 key=$2
-ansible-playbook -i /home/rooty/inventory.${hosts}.host --key-file /home/rooty/.ssh/$key /home/rooty/ansible/${hosts}/main.yaml
+ansible-playbook -i /home/rooty/inventory.${hosts}.host --key-file /home/rooty/.ssh/$key /home/rooty/ansible/_final/${hosts}/main.yaml
